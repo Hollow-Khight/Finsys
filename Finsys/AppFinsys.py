6ca30f9f23f345ee700a93c62236ec15a525870e
Hollow-Khight <jeshuahung936@gmail.com>
2025-10-24 17:49:26 -0400

    V1.3: Criei uma rota separada do index para organizar melhor o projeto e separar as futuras funcionalidades, mais a frente serão mudados os nomes para a identificação de forma mais clara.

--- Finsys/AppFinsys.py
+++ Finsys/AppFinsys.py
@@ -3,10 +3,15 @@ from flask import Flask
 AppFinsys = Flask (__name__)
 
 @AppFinsys.route('/')
-@AppFinsys.route('/ola')
+@AppFinsys.route('/index')
 
-def raiz():
+def indice():
     return 'Olá, Meu nome é Jeshua Daniel'
+
+@AppFinsys.route('/rota1')
+def rota1():
+    resposta = "<H3> Esta é uma rota criada e separada do index </H3>"
+    return resposta
 
 def saudacao (nome):
     return f'Olá, {nome}!'
