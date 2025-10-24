60d8158e0d95fb10b3f3f51a20056aba3060b34c
Hollow-Khight <jeshuahung936@gmail.com>
2025-10-24 19:01:50 -0400

    V1.5: Foram criados 2 html novos, o "index.html" e "usuario.html" conectados a suas devidas rotas no arquivo "AppFinsys", na rota onde é renderizado o "usuario.html" está sendo usado um dicionário para armazenar as informações do desenvolvedor e no "index.html" foram adicionados os links que levarão até algumas páginas da aplicação.

--- Finsys/AppFinsys.py
+++ Finsys/AppFinsys.py
@@ -6,6 +6,15 @@ AppFinsys = Flask (__name__, template_folder='templates')
 def home():
     return render_template ('home.html')
 
+@AppFinsys.route("/index")
+def indice():
+    return render_template ('index.html')
+
+@AppFinsys.route("/usuario")
+def dados_usuario():
+    dados_usu = {"nome": "Jeshua", "profissao": "Desenvolvedor", "aplicativo":"Finsys"}
+    return render_template ("usuario.html", dados = dados_usu)
+
 @AppFinsys.route("/contato")
 def contato():
     return render_template ('contato.html')
